46a163de176e066b89c2b7249b106a58c53308bb
sonam singh <s.singh2368@gmail.comm>
2020-05-06 07:44:49 +0530

    Commiting full project

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular App';
+  name = 'sonam singh';
+  testVar() {
+    const a = 5;
+    console.log(a);
+}
+}
+
+
+
+
+
+
